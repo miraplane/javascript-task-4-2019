549ce4f8908960014486fde04e9f8359829b7767
IrinaMochalova <IrinaMochalova@gmail.com>
2019-11-16 21:04:25 +0500

    Добавлены тесты

--- lib.spec.js
+++ lib.spec.js
@@ -50,12 +50,86 @@ const friends = [
     }
 ];
 
+const newListFriends = [
+    {
+        name: 'Sam',
+        friends: ['Sally', 'Alise'],
+        gender: 'male',
+        best: true
+    },
+    {
+        name: 'Sally',
+        friends: ['Sam', 'Mat'],
+        gender: 'female'
+    },
+    {
+        name: 'Mat',
+        friends: ['Sally', 'Alise'],
+        gender: 'male'
+    },
+    {
+        name: 'Alise',
+        friends: ['Mat', 'Sam'],
+        gender: 'female'
+    }
+];
+
+const bestFriends = [
+    {
+        name: 'Sam',
+        friends: [],
+        gender: 'male',
+        best: true
+    },
+    {
+        name: 'Sally',
+        friends: [],
+        gender: 'female',
+        best: true
+    },
+    {
+        name: 'Mat',
+        friends: [],
+        gender: 'male',
+        best: true
+    },
+    {
+        name: 'Alise',
+        friends: [],
+        gender: 'female',
+        best: true
+    }
+];
+
 describe('Итераторы', () => {
     it('должны обойти в правильном порядке друзей и составить пары', () => {
+        assert.deepStrictEqual(doFriendList(friends), [
+            [friend(friends, 'Sam'), friend(friends, 'Sally')],
+            [friend(friends, 'Brad'), friend(friends, 'Emily')],
+            [friend(friends, 'Mat'), friend(friends, 'Sharon')],
+            friend(friends, 'Julia')
+        ]);
+    });
+
+    it('должны обойти в правильном порядке граф с циклом', () => {
+        assert.deepStrictEqual(doFriendList(newListFriends), [
+            [friend(newListFriends, 'Sam'), friend(newListFriends, 'Alise')],
+            [friend(newListFriends, 'Mat'), friend(newListFriends, 'Sally')]
+        ]);
+    });
+
+    it('должны обойти в правильном порядке несвязный граф', () => {
+        assert.deepStrictEqual(doFriendList(bestFriends), [
+            [friend(bestFriends, 'Mat'), friend(bestFriends, 'Alise')],
+            [friend(bestFriends, 'Sam'), friend(bestFriends, 'Sally')]
+        ]);
+    });
+
+    function doFriendList(friendList) {
         const maleFilter = new lib.MaleFilter();
         const femaleFilter = new lib.FemaleFilter();
-        const maleIterator = new lib.LimitedIterator(friends, maleFilter, 2);
-        const femaleIterator = new lib.Iterator(friends, femaleFilter);
+        const maleIterator = new lib.LimitedIterator(friendList, maleFilter, 2);
+        const femaleIterator = new lib.Iterator(friendList, femaleFilter);
 
         const invitedFriends = [];
 
@@ -70,20 +144,15 @@ describe('Итераторы', () => {
             invitedFriends.push(femaleIterator.next());
         }
 
-        assert.deepStrictEqual(invitedFriends, [
-            [friend('Sam'), friend('Sally')],
-            [friend('Brad'), friend('Emily')],
-            [friend('Mat'), friend('Sharon')],
-            friend('Julia')
-        ]);
-    });
+        return invitedFriends;
+    }
 
-    function friend(name) {
-        let len = friends.length;
+    function friend(friendList, name) {
+        let len = friendList.length;
 
         while (len--) {
-            if (friends[len].name === name) {
-                return friends[len];
+            if (friendList[len].name === name) {
+                return friendList[len];
             }
         }
     }
